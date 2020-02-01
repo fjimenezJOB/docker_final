1b495b170635b5de828433c6fe7359c38f59544f
fjimenezJOB <fjimenezjob@gmail.com>
2020-02-01 13:40:42 +0100

    todo ok, falta dockerizar

--- src/main.py
+++ src/main.py
@@ -4,10 +4,11 @@ import os
 
 # docker-compose up para arrancar el proyecto desde el contenedor
 # SI HAY ALGUN CAMBIO GORDO EN MAIN TIENES QUE HACER OTRO BUILD:
-    # ( SI NOS PASA QUE DEJA DE FUNCIONAR )
+# ( SI NOS PASA QUE DEJA DE FUNCIONAR )
 
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -18,22 +19,22 @@ def index():
         form_irpf = round(float(request.form.get('irpf')), 2)
 
         # Calculos intermedios
-        cal01 = ((form_iva - form_irpf) /100) +1
+        cal01 = ((form_iva - form_irpf) / 100) + 1
         cal02 = form_total / cal01
-    
+
         # Calculos finales
         irpf = round((cal02 * (form_irpf / 100)), 2)
         iva = round((cal02 * (form_iva / 100)), 2)
 
-        subtotal = round(cal02 ,2)
-        limpio = round(form_total - iva,2)
+        subtotal = round(cal02, 2)
+        limpio = round(form_total - iva, 2)
 
         context = {
             'necesario': form_total,
-            'iva':iva,
-            'subtotal':subtotal,
-            'limpio' : limpio,
-            'irpf':irpf
+            'iva': iva,
+            'subtotal': subtotal,
+            'limpio': limpio,
+            'irpf': irpf
         }
 
         return render_template('index.html', **context)
@@ -42,6 +43,6 @@ def index():
 
 if __name__ == "__main__":
     # Esto es un problema porque no le podemos poner un puerto de salida, para eso vamos a crear lo siguiente:
-        # SI HAY VARIABLE DE ENTORNO PORT COJE ESA VARIABLE, SI NO COJE EL 5000
+    # SI HAY VARIABLE DE ENTORNO PORT COJE ESA VARIABLE, SI NO COJE EL 5000
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
